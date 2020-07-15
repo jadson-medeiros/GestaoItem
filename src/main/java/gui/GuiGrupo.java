dad3c018140db2c18bfd6c7a9ef7c92513aa50e7
Jadson Medeiros <developer.medeiros@gmail.com>
2020-07-14 22:13:16 -0300

    Add GrupoList

--- src/main/java/gui/GuiGrupo.java
+++ src/main/java/gui/GuiGrupo.java
@@ -4,27 +4,60 @@
  * and open the template in the editor.
  */
 package gui;
+import dao.GrupoDao;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.List;
+import javax.ejb.EJB;
+import model.Grupo;
 
 /**
  *
  * @author jadson
  */
-@Named(value = "guiLogin")
+@Named(value = "guiGrupo")
 @SessionScoped
-public class GuiLogin implements Serializable 
+public class GuiGrupo implements Serializable 
 {
     /**
      * Creates a new instance of GuiLogin
      */
     private String usuario;    
     private String senha; 
+    private List<Grupo> grupos;
+    private Grupo grupo;
+
+    @EJB
+    GrupoDao daoGrupo;
     
-    public GuiLogin() 
+    public GuiGrupo() 
     {
+        this.grupos = daoGrupo.getAllGrupo();
     }
+      
+    public Grupo getGrupo() 
+    {
+        return grupo;
+    }
+
+    public void setGrupo(Grupo grupo) 
+    {
+        this.grupo = grupo;
+    }
+    
+    
+    public List<Grupo> getGrupos()
+    {
+        return grupos;
+    }
+
+    public void setGrupos(List<Grupo> grupos) 
+    {
+        this.grupos = grupos;
+    }
+    
+  
     
     public String logar()
     {
