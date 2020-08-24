ab65b4cfecbc3a4a830a04d8266edc413f80703b
Jadson Medeiros <developer.medeiros@gmail.com>
2020-08-24 00:52:53 -0300

    cryptografia de senha | cadastro

--- src/main/java/gui/GuiCadastroUsuario.java
+++ src/main/java/gui/GuiCadastroUsuario.java
@@ -37,6 +37,38 @@ public class GuiCadastroUsuario implements Serializable
     {
     }  
 
+    public void addUsuario()
+    {
+        usuario.setSenha(encrypt(usuario.getUsuario(), usuario.getSenha()));
+        daoUsuario.add(usuario);
+    }
+     
+    public String encrypt(String login, String senha) 
+    {
+        String sign = senha;
+        try {
+            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+            md.update(sign.getBytes());
+            byte[] hash = md.digest();
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < hash.length; i++) 
+            {
+                if ((0xff & hash[i]) < 0x10) 
+                {
+                    hexString.append("0" + Integer.toHexString((0xFF & hash[i])));
+                } else {
+                    hexString.append(Integer.toHexString(0xFF & hash[i]));
+                }
+            }
+            
+            sign = hexString.toString();
+        } catch (Exception nsae) {
+            nsae.printStackTrace();
+        }
+        
+        return sign;
+    }
+    
     public UsuarioDao getDaoUsuario()
     {
         return daoUsuario;
@@ -62,20 +94,6 @@ public class GuiCadastroUsuario implements Serializable
         this.tipoUsuarioENUM = tipoUsuarioENUM;
     }
 
-    public void addUsuario()
-    {
-      //  if(incluindo)
-        //{
-            daoUsuario.add(usuario);
-        //} 
-       // else
-        //{
-        //    daoUsuario.update(usuario);
-        //}
-             
-        // return "./Cadastro.xhtml";
-    }
-     
     public List<Usuario> getUsuarios() 
     {
         return usuarios;
