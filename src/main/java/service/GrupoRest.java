a292e92c23ea9f25375b76a0642c203e79b5e6d8
Jadson Medeiros <developer.medeiros@gmail.com>
2020-11-13 20:02:50 -0300

    Add grupo and Item webservice

---
+++ src/main/java/service/GrupoRest.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import dao.UsuarioDao;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import model.Usuario;
+import org.primefaces.shaded.json.JSONObject;
+
+/**
+ *
+ * @author jadson
+ */
+
+@Stateless
+@Path("user")
+public class UsuarioRest  {
+    
+    @EJB
+    UsuarioDao daoUsuario;
+        
+    @POST
+    @Path("auth")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)    
+    public Response validarLogin(String res) 
+    {        
+        JSONObject response = new JSONObject(res);
+        String user = response.getString("user");
+        String password = response.getString("password");
+        Usuario usuario = daoUsuario.getUsuario(user);
+        
+        if(usuario == null){
+            response.put("token", "404");
+            return Response.status(404).entity(response.toString()).build();
+        }
+        
+        if (usuario.getSenha().equals(encrypt(user, password))) 
+        {
+            response.put("token", "200");
+            response.put("nome", usuario.getNome());
+            response.put("user", usuario.getUsuario());
+            return Response.status(200).entity(response.toString()).build();
+        }
+       
+        response.put("token", "404");
+        return Response.status(404).entity(response.toString()).build();
+    }
+    
+    private String encrypt(String login, String senha) 
+    {
+        String sign = senha;
+        try
+        {
+            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+            md.update(sign.getBytes());
+            byte[] hash = md.digest();
+            StringBuffer hexString = new StringBuffer();
+            
+            for (int i = 0; i < hash.length; i++) 
+            {
+                if ((0xff & hash[i]) < 0x10) 
+                {
+                    hexString.append("0" + Integer.toHexString((0xFF & hash[i])));
+                } else {
+                    hexString.append(Integer.toHexString(0xFF & hash[i]));
+                }
+            }
+            
+            sign = hexString.toString();
+        } catch (Exception nsae) 
+        {
+            nsae.printStackTrace();
+        }
+        
+        return sign;
+    }    
+}
