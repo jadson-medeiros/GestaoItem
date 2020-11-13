0c7ef5489834bb9fc0e9b0d94311036e249cde32
Jadson Medeiros <developer.medeiros@gmail.com>
2020-11-13 20:30:40 -0300

    get grupos list

--- src/main/java/service/GrupoRest.java
+++ src/main/java/service/GrupoRest.java
@@ -6,13 +6,16 @@
 package service;
 
 import dao.GrupoDao;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import model.Componente;
 import model.Grupo;
 import org.primefaces.shaded.json.JSONObject;
 
@@ -51,4 +54,29 @@ public class GrupoRest
         response.put("message", "Error ao criar o Grupo.");
         return Response.status(404).entity(response.toString()).build();
     }    
+    
+    @GET
+    @Path("grupos")
+    @Consumes(MediaType.APPLICATION_JSON)  
+    public Response getGrupos() 
+    {                
+        List<Grupo> grupoList = daoGrupo.getAllGrupos();
+        List<Grupo> newGrupoList = daoGrupo.getAllGrupos();
+        
+        for (Grupo g : grupoList)
+        {
+            if(g.getComponentes().size() > 0)
+            {
+                for (Componente c : g.getComponentes())
+                {
+                    if(newGrupoList.contains(c)) newGrupoList.remove(c);
+                }
+            }             
+        }
+                
+        JSONObject response = new JSONObject();
+        response.put("grupos", newGrupoList);
+        response.put("token", "200");
+        return Response.status(200).entity(response.toString()).build();
+    } 
 }
